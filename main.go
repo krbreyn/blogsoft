ffcef0b8ddfe496c0ff5e2cc3881882e9730a773
krbreyn <krbreyn@gmail.com>
2025-04-27 18:59:45 -0400

    restructuring

--- main.go
+++ main.go
@@ -1,76 +1,167 @@
 package main
 
 import (
-	"bufio"
 	_ "embed"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
-	"slices"
-	"strconv"
 	"strings"
 	"time"
 )
 
-//go:embed templates/base.tmpl
-var BaseTmpl string
-
-//go:embed templates/post.tmpl
-var PostTmpl string
-
-type Templates struct {
-	Base, Post *template.Template
-}
-
-type BlogStore interface {
-	Get(fillename string) (BlogPost, error)
-	GetAll() ([]BlogPost, error)
-	GetLastN(n int) ([]BlogPost, error)
-}
-
-type Cacher interface {
-	Is(name string) bool
-	Get(name string) BlogPost
-	Put(name string) BlogPost
-}
-
 type BlogServer struct {
-	s BlogStore
-	c Cacher
+	s *BlogStore
 }
 
-//var blog_dir = os.Getenv("BLOG_DIR")
+func (b *BlogServer) RenderWithBase(content template.HTML) (string, error) {
+	ret := &strings.Builder{}
+	templ, err := b.s.GetBaseTemplate()
+	if err != nil {
+		return "", err
+	}
+	err = templ.Execute(ret, struct {
+		Title       string
+		PageContent template.HTML
+		StyleSheet  template.CSS
+	}{
+		"BlogSoft", content, b.s.GetStyle(),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return ret.String(), nil
+}
+
+func (b *BlogServer) RenderIndex() (string, error) {
+	content, err := b.s.GetIndexPage()
+	if err != nil {
+		return "", fmt.Errorf("blog not properly set up: %w", err)
+	}
+
+	page_content := template.HTML(content)
+	ret, err := b.RenderWithBase(page_content)
+	if err != nil {
+		return "", nil
+	}
+	return ret, nil
+}
+
+func (b *BlogServer) RenderBlogPost(name string) (string, error) {
+	blog_post, err := b.s.GetBlog(name)
+	if err != nil {
+		return "", err
+	}
+	blog_templ, err := b.s.GetBlogPostTemplate()
+	if err != nil {
+		return "", err
+	}
+
+	post_buf := &strings.Builder{}
+	date_string := RenderDateString(blog_post.Date)
+	err = blog_templ.Execute(post_buf, struct {
+		Title   string
+		Date    string
+		Content template.HTML
+		Tags    []string
+	}{
+		blog_post.Title,
+		date_string,
+		template.HTML(blog_post.Content),
+		blog_post.Tags,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	page_content := template.HTML(post_buf.String())
+	ret, err := b.RenderWithBase(page_content)
+	if err != nil {
+		return "", nil
+	}
+	return ret, nil
+}
+
+func (b *BlogServer) RenderBlogList() (string, error) {
+	posts, err := b.s.GetAllBlogs()
+	if err != nil {
+		return "", err
+	}
+	templ, err := b.s.GetBlogListTemplate()
+	if err != nil {
+		return "", err
+	}
+
+	post_buf := &strings.Builder{}
+	err = templ.Execute(post_buf, struct {
+		Posts []BlogPost
+	}{
+		posts,
+	})
+	page_content := template.HTML(post_buf.String())
+	ret, err := b.RenderWithBase(page_content)
+	if err != nil {
+		return "", nil
+	}
+	return ret, nil
+}
 
 func main() {
-	base_t := template.New("base")
-	base_t = template.Must(base_t.Parse(BaseTmpl))
-	post_t := template.New("post")
-	post_t = template.Must(post_t.Parse(PostTmpl))
-
-	templates := Templates{base_t, post_t}
-
 	mux := http.NewServeMux()
+
+	serv := BlogServer{&BlogStore{}}
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/index/", http.StatusSeeOther)
 	})
 
 	mux.HandleFunc("/index/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte(RenderIndexPage(templates)))
-	})
-
-	mux.HandleFunc("/post/", func(w http.ResponseWriter, r *http.Request) {
-		target_post := strings.TrimPrefix(r.URL.Path, "/post/")
-		post, err := OpenBlogPost(target_post)
+		index, err := serv.RenderIndex()
 		if err != nil {
-			_, _ = w.Write([]byte(target_post))
+			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte(err.Error()))
-			http.NotFound(w, r)
 			return
 		}
-		_, _ = w.Write([]byte(RenderPostPage(post, templates)))
+		_, _ = w.Write([]byte(index))
+	})
+
+	mux.HandleFunc("/blog/", func(w http.ResponseWriter, r *http.Request) {
+		target_post := strings.TrimPrefix(r.URL.Path, "/blog/")
+
+		if target_post == "" {
+			ret, err := serv.RenderBlogList()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, _ = w.Write([]byte(err.Error()))
+			}
+			_, _ = w.Write([]byte(ret))
+			return
+		}
+
+		post, err := serv.RenderBlogPost(target_post)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		_, _ = w.Write([]byte(post))
+	})
+
+	mux.HandleFunc("/about/", func(w http.ResponseWriter, r *http.Request) {
+		ret, err := serv.s.GetAbout()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		ret, err = serv.RenderWithBase(template.HTML(ret))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+		}
+		_, _ = w.Write([]byte(ret))
 	})
 
 	srv := &http.Server{
@@ -87,199 +178,6 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type BlogPost struct {
-	Title    string
-	Filename string
-	Date     time.Time
-	Tags     []string
-	Content  string
-}
-
-func OpenBlogPost(filename string) (BlogPost, error) {
-	path := "./blog/" + filename + ".sbml"
-	_, err := os.Stat(path)
-	if err != nil {
-		return BlogPost{}, err
-	}
-
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return BlogPost{}, err
-	}
-
-	s := bufio.NewScanner(strings.NewReader(string(file)))
-
-	s.Scan()
-	title := s.Text()
-	s.Scan()
-	date := s.Text()
-	s.Scan()
-	var tags []string
-	tag_line := s.Text()
-	if tag_line != "" {
-		str := strings.TrimPrefix(tag_line, "[[tags: ")
-		if str != tag_line {
-			str := strings.TrimSuffix(str, "]]")
-			tags = strings.Fields(str)
-		}
-	}
-	s.Scan()
-	var content string
-	var text string
-	s.Scan()
-	text = s.Text()
-	for s.Scan() {
-		content += text + "\n<br>\n"
-		text = s.Text()
-	}
-	if text != "" {
-		content += text
-	}
-
-	if s.Err() != nil {
-		return BlogPost{}, err
-	}
-
-	t, err := time.Parse("1/2/2006", date)
-	if err != nil {
-		return BlogPost{}, err
-	}
-	return BlogPost{title, filename, t, tags, content}, nil
-}
-
-func OpenIndex() (string, error) {
-	path := "./blog/index.sbml"
-	_, err := os.Stat(path)
-	if err != nil {
-		return "", err
-	}
-
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-
-	s := bufio.NewScanner(strings.NewReader(string(file)))
-	var ret string
-	for s.Scan() {
-		text := s.Text()
-		if strings.HasPrefix(text, "[[blog_last_x") && strings.HasSuffix(text, "]]") {
-			split := strings.Fields(text)
-			if len(split) == 2 {
-				no, err := strconv.Atoi(strings.TrimSuffix(strings.Fields(text)[1], "]]"))
-				if err == nil {
-					text = BlogLastX(no)
-				}
-			}
-		}
-		ret += text + "\n"
-	}
-	return ret, nil
-}
-
-func GetStyle() template.CSS {
-	path := "./blog/style.css"
-	_, err := os.Stat(path)
-	if err != nil {
-		return ""
-	}
-
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return ""
-	}
-
-	return template.CSS(string(file))
-}
-
-// TODO properly
-func GetAllPosts() []BlogPost {
-	// var entries []string
-	// filepath.WalkDir("./blog/")
-	b1, _ := OpenBlogPost("test_post")
-	b2, _ := OpenBlogPost("test_2")
-	b3, _ := OpenBlogPost("test_3")
-	return []BlogPost{b1, b2, b3}
-}
-
-func BlogLastX(no int) string {
-	// lazy, unoptimized
-	// TODO open each file one by one and just check the date
-
-	posts := GetAllPosts()
-	slices.SortFunc(posts, func(i, j BlogPost) int {
-		if i.Date.After(j.Date) {
-			return -1
-		}
-		if i.Date.Before(j.Date) {
-			return 1
-		}
-		return 0
-	})
-	l := min(len(posts), no)
-	posts = posts[:l]
-	var ret string
-	for _, p := range posts {
-		ret += fmt.Sprintf(`<a href="/post/%s">%s</a><br>`, p.Filename, p.Title)
-		ret += "&emsp;"
-		ret += RenderDateString(p.Date)
-		ret += "<br>\n"
-	}
-	return ret
-}
-
-func RenderPostPage(post BlogPost, ts Templates) string {
-	b := &strings.Builder{}
-	p := &strings.Builder{}
-	date_string := RenderDateString(post.Date)
-	_ = ts.Post.Execute(p, struct {
-		Title   string
-		Date    string
-		Content template.HTML
-		Tags    []string
-	}{
-		post.Title,
-		date_string,
-		template.HTML(post.Content),
-		post.Tags,
-	})
-	page_content := template.HTML(p.String())
-	_ = ts.Base.Execute(b, struct {
-		Title       string
-		PageContent template.HTML
-		StyleSheet  template.CSS
-	}{
-		post.Title, page_content, GetStyle(),
-	})
-	return b.String()
-}
-
-func RenderIndexPage(ts Templates) string {
-	b := &strings.Builder{}
-	index, err := OpenIndex()
-	// hack?
-	// TODO CHECK TEMPLATES FOR ERRORS
-	if err == nil {
-		_ = ts.Base.Execute(b, struct {
-			Title       string
-			PageContent template.HTML
-			StyleSheet  template.CSS
-		}{
-			"BlogSoft", template.HTML(index), GetStyle(),
-		})
-	} else {
-		_ = ts.Base.Execute(b, struct {
-			Title       string
-			PageContent template.HTML
-			StyleSheet  template.CSS
-		}{
-			"BlogSoft", template.HTML("no index page found"), GetStyle(),
-		})
-	}
-
-	return b.String()
 }
 
 func RenderDateString(date time.Time) string {
--- simple_store.go
+++ simple_store.go
@@ -1,1 +1,207 @@
 package main
+
+import (
+	"bufio"
+	"html/template"
+	"os"
+	"slices"
+	"strings"
+	"time"
+)
+
+type BlogPost struct {
+	Title    string
+	Filename string
+	Date     time.Time
+	DateRepr string
+	Tags     []string
+	Content  string
+}
+
+type Cacher struct {
+}
+
+type BlogStore struct {
+	// c *Cacher
+}
+
+func (s *BlogStore) GetBlog(name string) (BlogPost, error) {
+	path := "./content/blog/" + name + ".blog"
+	_, err := os.Stat(path)
+	if err != nil {
+		return BlogPost{}, err
+	}
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return BlogPost{}, err
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(file)))
+
+	scanner.Scan()
+	title := scanner.Text()
+	scanner.Scan()
+	date := scanner.Text()
+	scanner.Scan()
+	var tags []string
+	tag_line := scanner.Text()
+	if tag_line != "" {
+		str := strings.TrimPrefix(tag_line, "[[tags: ")
+		if str != tag_line {
+			str := strings.TrimSuffix(str, "]]")
+			tags = strings.Fields(str)
+		}
+	}
+	scanner.Scan()
+	var content string
+	var text string
+	scanner.Scan()
+	text = scanner.Text()
+	for scanner.Scan() {
+		content += text + "\n<br>\n"
+		text = scanner.Text()
+	}
+	if text != "" {
+		content += text
+	}
+
+	if scanner.Err() != nil {
+		return BlogPost{}, err
+	}
+
+	t, err := time.Parse("1/2/2006", date)
+	if err != nil {
+		return BlogPost{}, err
+	}
+	return BlogPost{title, name, t, RenderDateString(t), tags, content}, nil
+
+}
+
+// TODO properly
+func (s *BlogStore) GetAllBlogs() ([]BlogPost, error) {
+	// var entries []string
+	// filepath.WalkDir("./blog/")
+	b1, _ := s.GetBlog("test_post")
+	b2, _ := s.GetBlog("test_2")
+	b3, _ := s.GetBlog("test_3")
+	return []BlogPost{b1, b2, b3}, nil
+}
+
+func (s *BlogStore) GetLastNBlogs(no int) ([]BlogPost, error) {
+	posts, err := s.GetAllBlogs()
+	if err != nil {
+		return nil, err
+	}
+	slices.SortFunc(posts, func(i, j BlogPost) int {
+		if i.Date.After(j.Date) {
+			return -1
+		}
+		if i.Date.Before(j.Date) {
+			return 1
+		}
+		return 0
+	})
+	l := min(len(posts), no)
+	return posts[:l], nil
+}
+
+func (s *BlogStore) GetAbout() (string, error) {
+	path := "./content/about.html"
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(file), nil
+}
+
+func (s *BlogStore) GetIndexPage() (template.HTML, error) {
+	path := "./content/index.html"
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	t := template.New("index")
+	t, err = t.Parse(string(file))
+	if err != nil {
+		return "", err
+	}
+
+	blogs, err := s.GetLastNBlogs(5)
+	if err != nil {
+		return "", err
+	}
+
+	ret := &strings.Builder{}
+	err = t.Execute(ret, struct {
+		LastPosts []BlogPost
+	}{
+		blogs,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return template.HTML(ret.String()), nil
+}
+
+func (s *BlogStore) GetBaseTemplate() (*template.Template, error) {
+	path := "./content/templates/_base.html"
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	t := template.New("base")
+	t, err = t.Parse(string(file))
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (s *BlogStore) GetBlogPostTemplate() (*template.Template, error) {
+	path := "./content/templates/blog_post.html"
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	t := template.New("base")
+	t, err = t.Parse(string(file))
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (s *BlogStore) GetBlogListTemplate() (*template.Template, error) {
+	path := "./content/templates/blog_list.html"
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	t := template.New("base")
+	t, err = t.Parse(string(file))
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+func (s *BlogStore) GetStyle() template.CSS {
+	path := "./content/style.css"
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+
+	return template.CSS(string(file))
+}
